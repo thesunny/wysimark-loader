8c982eb576a3bd5fadec490b208af57c45d9621f
Sunny Hirai <thesunny@gmail.com>
2020-12-21 13:50:21 -0800

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+import React from "react";
+declare type UseWysimarkReturnValue = {
+    initialMarkdown: string;
+    getMarkdownRef: React.MutableRefObject<(() => string) | undefined>;
+    getMarkdown: () => string;
+};
+/**
+ * This allows you to create the `wysimark` object that you passs into the
+ * `Wysimark` component.
+ *
+ * This object is important because it exposes the method `getMarkdown` which
+ * you need to extract the markdown.
+ */
+export declare function useEditor(initialMarkdown: string): UseWysimarkReturnValue;
+/**
+ * The lazily loaded Wysimark component to place in an app.
+ */
+export declare function Editor({ editor, minHeight, maxHeight, url, appName, folder, }: {
+    editor: UseWysimarkReturnValue;
+    minHeight?: number;
+    maxHeight?: number;
+    url?: string;
+    appName: string;
+    folder: string;
+}): JSX.Element;
+export {};
